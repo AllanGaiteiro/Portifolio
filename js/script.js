04a6d48d37d7aecd3921568d88dce26693258d8e
Allan de souza gaiteiro <allangaiteirofoe@gmail.com>
2020-05-04 14:48:58 -0300

    atualizaçao
    atualizai os dados

--- js/script.js
+++ js/script.js
@@ -1,20 +1,36 @@
-var body = document.querySelector('body')
-var bemVindo = document.getElementById('bemVindo')
-var sombra = document.getElementById('sombra')
-var data = new Date()
-var hora = data.getHours()
+/*
+var eventos = []
+var github = new evento('github')
+eventos.push(github)
 
-if (hora > 6 && hora < 12) {
-    body.style.backgroundImage = 'url(images/dia.jpg)'
-    bemVindo.innerText = 'Bom Dia'
-    sombra.innerText = 'Bom Dia'
 
-}else if(hora < 18){
-    body.style.backgroundImage = 'url(images/tarde.jpg)'
-    bemVindo.innerText = 'Boa Tarde'
-    sombra.innerText = 'Boa Tarde'
-}else{
-    body.style.backgroundImage = 'url(images/noite.jpg)'
-    bemVindo.innerText = 'Boa Noite'
-    sombra.innerText = 'Boa Noite'
+var evento = function (a) {
+    this.elemento = a
+    this.elemento = elemento
+    this.mouseOut = document.getElementById(this.elemento).addEventListener('mouseenter', function () 
+    {
+        this.style.transform = 'scale(1.1,1.1)'
+    })
+    this.mouseEnter = document.getElementById(this.elemento).addEventListener('mouseout', function () {
+        this.style.transform = 'scale(1.0,1.0)'
+        
+    })
+
+}*/
+var mouseEnter = []
+var mouseOut = []
+var eventScal = 
+[
+    'github',
+    'face',
+    'linkedin'
+] 
+for (var i in eventScal) {
+    var nome = eventScal[i]
+    mouseEnter[i] = document.getElementById(nome).addEventListener('mouseenter',function (){
+        this.style.transform = 'scale(1.1,1.1)'
+    })
+    mouseOut[i] = document.getElementById(nome).addEventListener('mouseout', function(){
+        this.style.transform = 'scale(1.0,1.0)'
+    })
 }
